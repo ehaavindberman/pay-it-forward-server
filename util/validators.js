72a305feaeba1409b21ba2c21050b1d5289af163
Eric <ehaavindberman@gmail.com>
2020-12-01 14:51:40 -0800

    updated validators to have separate functions for each checkand increased password requirements

--- util/validators.js
+++ util/validators.js
@@ -1,3 +1,51 @@
+const validateRegisterUsername = (username) => {
+  if(username.trim() === ''){
+    return 'Username must not be empty';
+  }
+  if(username.length > 30) {
+    return 'Username must not be more than 30 characters';
+  }
+  return '';
+};
+
+const validateRegisterEmail = (email) => {
+  if(email.trim() === ''){
+    return 'Email must not be empty';
+  }
+
+  const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+  if(!email.match(regEx)){
+    return 'Email must be a valid email address';
+  }
+  return '';
+}
+
+const validateRegisterPassword = (password, confirmPassword) => {
+  if (password === '') {
+    return 'Password must not be empty';
+  }
+  if (password.length < 6) {
+    return 'Password must be at least 6 characters long';
+  }
+  const lowersRegex = /[a-z]/;
+  const capitalsRegex = /[A-Z]/;
+  const numbersRegex = /[0-9]/;
+  if (password.length < 20 && (!password.match(lowers) || !password.match(capitalsRegex) || !password.match(numbersRegex))) {
+    return 'Password must be at least 20 characters or use lower case, capital letters, and numbers'
+  }
+
+  if(password !== confirmPassword){
+    return 'Passwords must match';
+  }
+
+  return '';
+}
+
+const checkEmpty = (value) => {
+  return value.trim().length === 0;
+}
+
+
 module.exports.validateRegisterInput = (
   username,
   email,
@@ -5,23 +53,20 @@ module.exports.validateRegisterInput = (
   confirmPassword
 ) => {
   const errors = {};
-  if(username.trim() === ''){
-    errors.username = 'Username must not be empty';
+
+  const usernameError = validateRegisterUsername(username);
+  if (usernameError.length > 0) {
+    errors.username = usernameError;
   }
-  if(email.trim() === ''){
-    errors.email = 'Email must not be empty';
-  } else {
-    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-    if(!email.match(regEx)){
-      errors.email = 'Email must be a valid email address';
-    }
+
+  const emailError = validateRegisterEmail(email);
+  if (emailError.length > 0) {
+    errors.email = emailError;
   }
-  if (password === '') {
-    errors.password = 'Password must not be empty';
-  } else if (password.length < 6) {
-    errors.password = 'Password must be at least 6 characters long';
-  } else if(password !== confirmPassword){
-    errors.confirmPassword = 'Passwords must match';
+
+  const passwordError = validateRegisterPassword(password, confirmPassword);
+  if (passwordError.length > 0) {
+    errors.password = passwordError;
   }
 
   return {
@@ -30,12 +75,15 @@ module.exports.validateRegisterInput = (
   };
 };
 
+
+
 module.exports.validateLoginInput = (username, password) => {
   const errors = {};
-  if(username.trim() === ''){
+
+  if(checkEmpty(username)){
     errors.username = 'Username must not be empty';
   }
-  if(password.trim() === ''){
+  if(checkEmpty(password)){
     errors.password = 'Password must not be empty';
   }
 
@@ -45,15 +93,13 @@ module.exports.validateLoginInput = (username, password) => {
   };
 }
 
-module.exports.validateRecoInput = (
-  text, link, tag
-) => {
+module.exports.validateRecoInput = (text, link, tag) => {
   const errors = {};
-  // text, link, tag, description
-  if (text.trim() === '') {
+
+  if (checkEmpty(text)) {
     errors.text = 'Recommendation must not be empty'
   }
-  if (tag.trim() === '') {
+  if (checkEmpty(tag)) {
     errors.tag = 'Must contain one category'
   }
 
